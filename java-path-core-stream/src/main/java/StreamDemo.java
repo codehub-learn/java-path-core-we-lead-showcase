0cd69216114d496aafbe558d2afed8599eb0e947
dPetrotos <dpetrotos@outlook.com>
2023-11-13 17:28:46 +0200

    Added StreamDemo class

---
+++ java-path-core-stream/src/main/java/StreamDemo.java
@@ -0,0 +1,15 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+import com.thedeanda.lorem.Lorem;
+import com.thedeanda.lorem.LoremIpsum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StreamDemo {
+    private static final String LINE_DELIMITER = "---------------------------------------";
+    private static final Logger logger = LoggerFactory.getLogger(StreamDemo.class);
+    private static final Lorem generator = LoremIpsum.getInstance();
+
+    public static void main(String[] args) {
+
+    }
+}
